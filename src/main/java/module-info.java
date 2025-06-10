8e3dfa3182cd9798683df22417d8f0bf1af2fc33
Anna Quézia <142363490+annaquezia@users.noreply.github.com>
2025-06-10 19:52:25 -0300

    fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,6 @@ module com.littlecats.acalentoong {
 
     exports com.littlecats.acalentoong;
     opens com.littlecats.acalentoong to javafx.fxml;
-    exports com.littlecats.acalentoong.controller;
-    opens com.littlecats.acalentoong.controller to javafx.fxml;
     exports com.littlecats.acalentoong.models;
     opens com.littlecats.acalentoong.models to javafx.fxml;
 }
